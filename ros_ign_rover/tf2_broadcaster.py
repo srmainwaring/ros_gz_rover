d0dd8b2d7bc6d1dd2c7d4764e1301f36130d24f0
Rhys Mainwaring <rhys.mainwaring@me.com>
2022-04-05 17:26:41 +0100

    Create new ros2 package
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

---
+++ ros_ign_rover/tf2_broadcaster.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from ros_ign_rover.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'ros_ign_rover'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='rhys',
+    maintainer_email='rhys.mainwaring@me.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'tf2_broadcaster = ros_ign_rover.tf2_broadcaster:main'
+        ],
+    },
+)
